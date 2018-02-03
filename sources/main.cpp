9b51211f521e186ad3ca6bb6d14d53a1964e66c3
DESKTOP-43BFKRJ\ilkun <ilkunviktor@gmail.com>
2018-02-03 23:06:02 +0200

    + added first normal commit changes

--- sources/main.cpp
+++ sources/main.cpp
@@ -15,36 +15,66 @@ int main()
 	bool b = inputFile.is_open();
 	assert(inputFile.is_open());
 
-	int rows = 0; // 1 ≤ number of rows ≤ 10000
-	int columns = 0; // 1 ≤ number of columns ≤ 10000
-	int drones = 0; // 1 ≤ D ≤ 1000
-	int turns = 0; // 1 ≤ deadline of the simulation ≤ 1000000
-	int payload = 0; // 1 ≤ maximum load of a drone ≤ 10000
-	int productTypes = 0; // 1 ≤ P ≤ 10000
-	vector<int> productWeights; // 1 ≤ weight ≤ maximum load of a drone 
+	// 1 ≤ number of rows ≤ 10000
+	// 1 ≤ number of columns ≤ 10000
+	// 1 ≤ D drones ≤ 1000
+	// 1 ≤ maximum load of a drone ≤ 10000
+	// 1 ≤ P product types ≤ 10000
+	// 1 ≤ W warehouses ≤ 10000
+	// value of products by type in warehouse => 0 ≤ number of items ≤ 10000
+	using uint_t = uint16_t; 
+	// 1 ≤ deadline of the simulation ≤ 1000000
+	using turns_t = uint32_t;
 
-	struct warehouse
+	uint_t rows = 0;
+	uint_t columns = 0;
+	uint_t drones = 0; 
+	turns_t turns = 0;
+	uint_t payload = 0;
+	uint_t productTypesCount = 0;
+	vector<uint_t> productWeights; // 1 ≤ weight ≤ maximum load of a drone
+	uint_t warehousesCount = 0;
+
+	struct Warehouse
 	{
-		int row = 0;
-		int column = 0;
-		vector<int> productsCounts;
+		uint_t row = 0;
+		uint_t column = 0;
+		vector<uint_t> productsCounts; // id => type, value => number of items
 	};
 
-	inputFile >> rows >> columns >> drones >> turns >> payload;
-	inputFile >> productTypes;
+	vector<Warehouse> warehouses;
 
-	for (int i = 0; i < productTypes; ++i)
+	inputFile >> rows >> columns >> drones >> turns >> payload;
+	inputFile >> productTypesCount;
+
+	for (uint_t i = 0; i < productTypesCount; ++i)
 	{
-		int productWeight = 0;
+		uint_t productWeight = 0;
 		inputFile >> productWeight;
 		productWeights.emplace_back(productWeight);
+	}
+
+	inputFile >> warehousesCount;
+
+	for (uint_t i = 0; i < warehousesCount; ++i)
+	{
+		Warehouse warehouse;
+		inputFile >> warehouse.row >> warehouse.column;
+
+		for (uint_t j = 0; j < productTypesCount; ++j)
+		{
+			uint_t productTypeCount = 0;
+			inputFile >> productTypeCount;
+			warehouse.productsCounts.emplace_back(productTypeCount);
+		}
+
+		warehouses.emplace_back(warehouse);
 	}
 
 	inputFile.close();
 
 	// solve
 
-	vector<warehouse> warehouses;
 
 	//output
 	ofstream resultFile;
